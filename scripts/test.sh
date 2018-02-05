caade55381e249bfae98357c71363ad9a5c7293c
Amir <amir.zaidi@dell.com>
2018-02-05 14:59:05 -0700

    all modified files

---
+++ scripts/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Activate virtual environment
+. /appenv/bin/activate
+
+# Download requirement to build cache
+pip download -d /build -r requirements_test.txt --no-input
+
+# Install application test requirement
+pip install --no-index -f /build -r requirements_test.txt
+
+# Run test.sh arguments
+exec $@
+
